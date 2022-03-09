00de19a2e3ec32ca8432bc0808a624e62506288c
Arthur Diesel <arthurdiesel@outlook.com>
2022-03-09 10:58:28 -0300

    CRUD!

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center">🚀 RestifyServer</h1>
+<h2 align="center">Servidor com CRUD utilizando Restify e um banco de dados SQLite.</h2>
+
+[![Run in Postman](https://run.pstmn.io/button.svg)](https://app.getpostman.com/run-collection/5e15de9bbd12322257eb?action=collection%2Fimport)
--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-const restify = require('restify');
-const errs = require('restify-errors');
+const restify = require('restify')
 
-const server = restify.createServer();
+const knex = require('knex')({
+    client: 'better-sqlite3',
+    connection: {
+      filename: "./db/db.db"
+    }
+});
+
+const server = restify.createServer()
 const port = 8080;
 
+server
+.use(restify.plugins.bodyParser())
+
 server.use((req, res, next) => {
+    console.log(req.route.spec)
     console.log(new Date)
     next()
 })
 
 server.get('/', (req, res, next) => {
-    res.send('Hello!')
+    res.send(200, {message: 'Hello World!', source: 'https://github.com/Arthur-Diesel/RestifyServer'})
 })
 
-server.get('/hello/:name', (req, res, next) => {
-    res.send('Hello ' + req.params.name + '!');
-});
-
-server.get('/error', (req, res, next) => {
-    return next(new errs.InternalServerError('boom!'));
+server.get('/tasks', async (req, res, next) => {
+    res.send(200, {data: await knex.select('*').from('tasks')})
 })
 
-server.on('InternalServer', function(req, res, err, callback) {
-    console.log(err)
-    return res.send('Error!')    
+server.get('/tasks/:id', async (req, res, next) => {
+    res.send(200, {data: await knex('tasks').where('id', req.params.id)})
+})
+
+server.post('/tasks', async (req, res, next ) => { 
+    const response = await knex('tasks').insert(req.body)
+    res.send(201, {data: response})
+})
+
+server.put('/tasks/:id', async (req, res, next) => {
+    const response = await knex('tasks').where('id', req.params.id).update(req.body)
+    res.send(200, {data: response})
+})
+
+server.del('/tasks/:id', async (req, res, next) => {
+    const response = await knex('tasks').where('id', req.params.id).del()
+    res.send(200, {data: response})
 })
 
 server.listen(port, () => console.log(`Listening at: http://localhost:` + port))
